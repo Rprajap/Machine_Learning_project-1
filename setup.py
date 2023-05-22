9cafd06b496ef2dcd70399f9ea8773394e9fff61
Rprajap <ramdasprajapati460@gail.com>
2023-05-22 20:37:12 +0530

    Sending changes As on dt 22.05.2023  to github

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+
+
+# Declarig variable for setup function
+PROJRCT_NAME='housing-pridictor'
+VERSION='0.0.1'
+AUTHOR='Ramdas'
+DESCRIPTION='This is first fsds-nov Machine Learnig Project'
+
+
+setup(name='Housing-predictor',
+      version='0.0.1',
+      author='Ramdas',
+      description='This is first fsds-nov Machine Learnig Project',
+      packages=['housing'])
+       #instatall_requires=get_requirements_list() )
